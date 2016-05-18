4e5d3852f15a6f357c105691b5a55e78014550dd
mwl <mwl@trifork.com>
2016-05-18 21:08:46 +0100

    First attempt at getting Tansu to run in Mesos

---
+++ src/main/java/dk/mwl/mesos/tansu/TansuScheduler.java
@@ -0,0 +1,12 @@
+package dk.mwl.mesos.tansu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TansuScheduler {
+    public static void main(String[] args) {
+        SpringApplication.run(TansuScheduler.class, args);
+    }
+
+}
